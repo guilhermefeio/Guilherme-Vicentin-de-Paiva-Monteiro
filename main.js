b1aab2c24c4d83768368eb3edcaf9670a66ce72e
guilhermefeio <148064776+guilhermefeio@users.noreply.github.com>
2024-05-02 07:56:47 -0300

    .

--- main.js
+++ main.js
@@ -48,10 +48,12 @@ function atualizaCronometro(){
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
     for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
-    }
-}
-
+         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+     }
+ }
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
